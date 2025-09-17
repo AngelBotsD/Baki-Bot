6e5f6efcf8ef85a977b8976837265cba1ee2b96e
AngelBotsD <angelperez22448@gmail.com>
2025-09-16 22:47:33 -0600

    Actualizar group-tagall².js

--- plugins/group-tagall².js
+++ plugins/group-tagall².js
@@ -1,27 +1,29 @@
-const handler = async (m, { conn, participants, isAdmin, isOwner }) => {
-  if (!m.isGroup) return;
-  if (!isAdmin && !isOwner) return global.dfail?.('admin', m, conn);
+const handler = async (m, { conn, participants, isAdmin, isOwner, isBotAdmin }) => {
+  if (!m.isGroup) return global.dfail?.('group', m, conn)
+  if (!isAdmin && !isOwner) return global.dfail?.('admin', m, conn)
+  if (!isBotAdmin) return global.dfail?.('botAdmin', m, conn)
 
-  const total = participants.length;
-  let texto = `*!  MENCION GENERAL  !*\n`;
-  texto += `   *PARA ${total} MIEMBROS* 🔊\n\n`;
+  const total = participants.length
+  let texto = `*!  MENCION GENERAL  !*\n`
+  texto += `   *PARA ${total} MIEMBROS* 🔊\n\n`
 
   for (const user of participants) {
-    const numero = user.id.split('@')[0];
-    texto += `┊» 🔊 @${numero}\n`;
+    const numero = user.id.split('@')[0]
+    texto += `┊» 🔊 @${numero}\n`
   }
 
-  await conn.sendMessage(m.chat, { react: { text: '🔊', key: m.key } });
+  await conn.sendMessage(m.chat, { react: { text: '🔊', key: m.key } })
 
   await conn.sendMessage(m.chat, {
     text: texto,
     mentions: participants.map(p => p.id)
-  }, { quoted: m });
-};
+  }, { quoted: m })
+}
 
-handler.customPrefix = /^\.?(todos|invocar|invocacion|invocación)$/i;
-handler.command = new RegExp();
-handler.group = true;
-handler.admin = true;
+handler.customPrefix = /^\.?(todos|invocar|invocacion|invocación)$/i
+handler.command = new RegExp()
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 
-export default handler;
+export default handler
