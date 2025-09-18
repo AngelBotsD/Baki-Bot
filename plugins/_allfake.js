588edf419664aa4fdcc276c4050cd3cfbc116c20
AngelBotsD <angelperez22448@gmail.com>
2025-09-17 21:00:53 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -1,58 +1,20 @@
-import fetch from 'node-fetch'
-
-export async function before(m, { conn }) {
-    // Descargamos la imagen desde la URL
-    const imgUrl = 'https://files.catbox.moe/jfov52.jpg'
-    const thumb = await (await fetch(imgUrl)).buffer()
-
-    // Mensaje fake
-    global.fake = {
-        contextInfo: {
-            isForwarded: true,
-            forwardedNewsletterMessageInfo: {
-                newsletterJid: "",
-                serverMessageId: 100,
-                newsletterName: '',
-            },
-            externalAdReply: {
-                title: botname,
-                body: 'Hola',
-                thumbnail: thumb,  // 👈 Funciona en WA normal y Business
-                sourceUrl: canal
-            }
-        }
+global.rcanal = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "",
+      serverMessageId: 100,
+      newsletterName: '',
+    },
+    externalAdReply: { 
+      showAdAttribution: true,
+      title: '𝗕𝗨𝗨 - 𝘽𝙊𝙏',
+      body: '',
+      previewType: "PHOTO",
+      thumbnailUrl: 'https://files.catbox.moe/jfov52.jpg', // 👈 tu link de imagen
+      sourceUrl: canal, // 👈 link clickeable
+      mediaType: 1,
+      renderLargerThumbnail: false
     }
-
-    // adReply
-    global.adReply = {
-        contextInfo: {
-            forwardingScore: 9999,
-            isForwarded: false,
-            externalAdReply: {
-                title: botname,
-                body: textbot,
-                thumbnail: thumb, // 👈 Funciona en WA normal y Business
-                sourceUrl: canal
-            }
-        }
-    }
-
-    // rcanal
-    global.rcanal = {
-        contextInfo: {
-            isForwarded: true,
-            forwardedNewsletterMessageInfo: {
-                newsletterJid: "",
-                serverMessageId: 100,
-                newsletterName: '',
-            },
-            externalAdReply: {
-                title: '𝗕𝗔𝗞𝗜 - 𝗕𝗢𝗧',
-                body: '',
-                thumbnail: thumb, // 👈 Funciona en WA normal y Business
-                sourceUrl: '',
-                showAdAttribution: true
-            }
-        }
-    }
+  }
 }
