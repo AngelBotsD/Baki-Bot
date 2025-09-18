883d7505c966b6269f6d6f23b57e99e3a5092588
AngelBotsD <angelperez22448@gmail.com>
2025-09-17 20:53:12 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -1,71 +1,51 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-  let img = await (await fetch('https://files.catbox.moe/jfov52.jpg')).buffer()
+    // Descargamos la imagen que servirá como "thumbnail"
+    let img = await (await fetch('https://files.catbox.moe/jfov52.jpg')).buffer()
 
-  // Fake normal (con thumbnail directo, visible en WhatsApp normal y Business)
-  global.fake = {
-    contextInfo: {
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: "",
-        serverMessageId: 100,
-        newsletterName: '',
-      },
-      externalAdReply: {
-        showAdAttribution: true,
-        title: botname,
-        body: 'Hola',
-        previewType: "PHOTO",
-        thumbnailUrl: 'https://files.catbox.moe/jfov52.jpg',
-        sourceUrl: canal,
-        mediaType: 1,
-        renderLargerThumbnail: false,
-        thumbnail: img, // 👈 Forzamos que se vea en WA normal
-        jpegThumbnail: img // 👈 Alternativa, asegura compatibilidad
-      }
+    // Mensaje fake que se enviará con imagen + texto
+    global.fake = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: "",
+                serverMessageId: 100,
+                newsletterName: '',
+            },
+            // Mandamos la imagen directamente
+            thumbnail: img,
+            jpegThumbnail: img,
+        },
+        caption: 'Hola', // Texto que quieras mostrar
+        image: img // Esto fuerza que se vea en WhatsApp normal y Business
     }
-  }
 
-  // adReply con imagen directa también
-  global.adReply = {
-    contextInfo: { 
-      forwardingScore: 9999, 
-      isForwarded: false, 
-      externalAdReply: {
-        showAdAttribution: true,
-        title: botname,
-        body: textbot,
-        previewType: "PHOTO",
-        sourceUrl: canal,
-        mediaType: 1,
-        renderLargerThumbnail: true,
-        thumbnail: img, // 👈 visible en normal
-        jpegThumbnail: img
-      }
+    // adReply con la misma lógica
+    global.adReply = {
+        contextInfo: {
+            forwardingScore: 9999,
+            isForwarded: false,
+            thumbnail: img,
+            jpegThumbnail: img,
+        },
+        caption: textbot,
+        image: img,
     }
-  }
 
-  // Canal
-  global.rcanal = {
-    contextInfo: {
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: "",
-        serverMessageId: 100,
-        newsletterName: '',
-      },
-      externalAdReply: { 
-        showAdAttribution: true,
-        title: '𝗕𝗔𝗞𝗜 - 𝗕𝗢𝗧',
-        body: '',
-        previewType: "PHOTO",
-        sourceUrl: '',
-        mediaType: 1,
-        renderLargerThumbnail: false,
-        thumbnail: img, // 👈 se verá igual
-        jpegThumbnail: img
-      }
+    // Canal con misma compatibilidad
+    global.rcanal = {
+        contextInfo: {
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: "",
+                serverMessageId: 100,
+                newsletterName: '',
+            },
+            thumbnail: img,
+            jpegThumbnail: img,
+        },
+        caption: '𝗕𝗔𝗞𝗜 - 𝗕𝗢𝗧', 
+        image: img,
     }
-  }
 }
