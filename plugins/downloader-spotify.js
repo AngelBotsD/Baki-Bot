253cb60025779ca677edd20e96902d5ca16bd42e
AngelBotsD <angelperez22448@gmail.com>
2025-09-19 16:22:45 -0600

    Actualizar downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -5,8 +5,7 @@ const apis = {
   delirius: 'https://delirius-apiofc.vercel.app/',
   siputzx: 'https://api.siputzx.my.id/api/',
   ryzen: 'https://apidl.asepharyana.cloud/',
-  rioo: 'https://restapi.apibotwa.biz.id/',
-  random1: 'https://api.agungny.my.id/api/'
+  rioo: 'https://restapi.apibotwa.biz.id/'
 };
 
 const handler = async (m, { conn, command, args, text, usedPrefix }) => {
@@ -14,8 +13,8 @@ const handler = async (m, { conn, command, args, text, usedPrefix }) => {
   if (!text) return m.reply(`_*[ ⚠️ ] Agrega lo que quieres Descargar en Spotify*_\n\n_Ejemplo:_\n.play Chica Paranormal.`);
 
   try {
-    // Usar primero random1
-    let { data } = await axios.get(`${apis.random1}spotify/search?query=${encodeURIComponent(text)}&limit=10`);
+    // Usar primero siputzx
+    let { data } = await axios.get(`${apis.siputzx}spotify/search?query=${encodeURIComponent(text)}&limit=10`);
 
     if (!data.data || data.data.length === 0) {
       throw `_*[ ⚠️ ] No se encontraron resultados para "${text}" en Spotify.*_`;
@@ -32,7 +31,7 @@ const handler = async (m, { conn, command, args, text, usedPrefix }) => {
 
     //＼／＼／＼／＼／＼／ DESCARGAR ＼／＼／＼／＼／＼／
     try {
-      const api1 = `${apis.random1}spotify/download?url=${encodeURIComponent(url)}`;
+      const api1 = `${apis.siputzx}spotify/download?url=${encodeURIComponent(url)}`;
       const response1 = await fetch(api1);
       const result1 = await response1.json();
 
@@ -50,34 +49,24 @@ const handler = async (m, { conn, command, args, text, usedPrefix }) => {
 
       } catch (e2) {
         try {
-          const api3 = `${apis.delirius}download/spotifydlv3?url=${encodeURIComponent(url)}`;
+          const api3 = `${apis.rioo}api/spotify?url=${encodeURIComponent(url)}`;
           const response3 = await fetch(api3);
           const result3 = await response3.json();
 
-          const downloadUrl3 = result3.data.url;
+          const downloadUrl3 = result3.data.response;
           await conn.sendMessage(m.chat, { audio: { url: downloadUrl3 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
 
         } catch (e3) {
           try {
-            const api4 = `${apis.rioo}api/spotify?url=${encodeURIComponent(url)}`;
+            const api4 = `${apis.ryzen}api/downloader/spotify?url=${encodeURIComponent(url)}`;
             const response4 = await fetch(api4);
             const result4 = await response4.json();
 
-            const downloadUrl4 = result4.data.response;
+            const downloadUrl4 = result4.link;
             await conn.sendMessage(m.chat, { audio: { url: downloadUrl4 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
 
           } catch (e4) {
-            try {
-              const api5 = `${apis.ryzen}api/downloader/spotify?url=${encodeURIComponent(url)}`;
-              const response5 = await fetch(api5);
-              const result5 = await response5.json();
-
-              const downloadUrl5 = result5.link;
-              await conn.sendMessage(m.chat, { audio: { url: downloadUrl5 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
-
-            } catch (e5) {
-              m.reply(`❌ Ocurrió un error al descargar el audio\nError:${e5.message}`);
-            }
+            m.reply(`❌ Ocurrió un error al descargar el audio\nError:${e4.message}`);
           }
         }
       }
