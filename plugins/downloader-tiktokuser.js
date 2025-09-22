3ee97576600dd7159b286e8e523b5c9b4a4cc47a
AngelBotsD <angelperez22448@gmail.com>
2025-09-22 08:57:03 -0600

    Actualizar downloader-tiktokuser.js

--- plugins/downloader-tiktokuser.js
+++ plugins/downloader-tiktokuser.js
@@ -62,7 +62,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ['play'];
+handler.command = handler.help = ['pl'];
 handler.tags = ['descargas'];
 handler.group = true;
 
