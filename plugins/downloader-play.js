a5e3a7041be19733e1edf0f96c6069f1c14a8831
AngelBotsD <angelperez22448@gmail.com>
2025-09-22 19:27:27 -0600

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -48,7 +48,7 @@ try {
 for (const q of posibles) {
 const apiUrl = urlBuilder(q)
 const r = await axios.get(apiUrl, {
-timeout: 60000,
+timeout: 25000,
 signal: controller.signal
 })
 if (r.data?.status && (r.data?.result?.url || r.data?.data?.url)) {
