d10e6122b584040cf1e8afcfaea399d164daa0c6
AngelBotsD <angelperez22448@gmail.com>
2025-09-25 19:27:18 -0600

    Actualizar group-demote³.js

--- plugins/group-demote³.js
+++ plugins/group-demote³.js
@@ -1,1 +1,32 @@
-xd
+let handler = async (m, { conn }) => {
+  const body = m.text?.trim()
+  let user
+
+  if (/^(promote|\.promote)\s+@/i.test(body)) {
+    const mentioned = m.mentionedJid || m.message?.extendedTextMessage?.contextInfo?.mentionedJid
+    if (!mentioned || !mentioned.length) 
+      return conn.sendMessage(m.chat, { react: { text: '☁️', key: m.key } })
+    user = mentioned[0]
+  } 
+  else if (/^(promote|\.promote)$/i.test(body)) {
+    user = m.quoted?.sender
+    if (!user) 
+      return conn.sendMessage(m.chat, { react: { text: '☁️', key: m.key } })
+  } else return
+
+  user = user.trim()
+  const metadata = await conn.groupMetadata(m.chat)
+  const admins = metadata.participants.filter(p => p.admin !== null).map(p => p.id)
+
+  if (admins.includes(user)) return
+
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
+    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
+  } catch (e) {}
+}
+
+handler.customPrefix = /^\.?(promote)$/i;
+handler.command = new RegExp()
+
+export default handler
