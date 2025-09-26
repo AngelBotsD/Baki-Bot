681a8f6b4e3ffefbc35adc29dfe726a8bea40f12
AngelBotsD <angelperez22448@gmail.com>
2025-09-25 22:10:20 -0600

    Actualizar group-delete².js

--- plugins/group-delete².js
+++ plugins/group-delete².js
@@ -10,6 +10,7 @@ return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 }
 }
 handler.customPrefix = /^\.?(del)$/i;
+handler.command = new RegExp()
 handler.group = true
 handler.admin = true
 
