4e171ac14e414fb0abd846e38e441fcc8b4218a4
AngelBotsD <angelperez22448@gmail.com>
2025-09-25 19:31:42 -0600

    Actualizar group-demote³.js

--- plugins/group-demote³.js
+++ plugins/group-demote³.js
@@ -2,17 +2,15 @@ let handler = async (m, { conn }) => {
   const body = m.text?.trim()
   let user
 
-  if (/^(promote|\.promote)\s+@/i.test(body)) {
-    const mentioned = m.mentionedJid || m.message?.extendedTextMessage?.contextInfo?.mentionedJid
-    if (!mentioned || !mentioned.length) 
-      return conn.sendMessage(m.chat, { react: { text: '☁️', key: m.key } })
-    user = mentioned[0]
+  // Si hay alguien mencionado
+  if (m.mentionedJid && m.mentionedJid.length) {
+    user = m.mentionedJid[0]
   } 
-  else if (/^(promote|\.promote)$/i.test(body)) {
-    user = m.quoted?.sender
-    if (!user) 
-      return conn.sendMessage(m.chat, { react: { text: '☁️', key: m.key } })
-  } else return
+  // Si es un reply
+  else if (/^\.?promote$/i.test(body) && m.quoted) {
+    user = m.quoted.sender
+  } 
+  else return conn.sendMessage(m.chat, { react: { text: '☁️', key: m.key } })
 
   user = user.trim()
   const metadata = await conn.groupMetadata(m.chat)
@@ -26,7 +24,7 @@ let handler = async (m, { conn }) => {
   } catch (e) {}
 }
 
-handler.customPrefix = /^\.?(promote)$/i;
+handler.customPrefix = /^\.?(promote)$/i
 handler.command = new RegExp()
 
 export default handler
